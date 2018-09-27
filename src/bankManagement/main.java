e837283fe604c173a90aefc39c3162b037e742ef
vam1193 <krrishvamsi7@gmail.com>
2018-09-26 23:42:08 -0400

    Initial commit

---
+++ src/bankManagement/main.java
@@ -0,0 +1,16 @@
+package bankManagement;
+import view.view;
+
+
+
+public class main {
+	
+	
+	public static void main(String[] args) {
+		view obj = new view();
+		main object = new main();
+	
+
+	}
+}
+	
